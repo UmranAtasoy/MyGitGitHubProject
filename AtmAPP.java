2bf6fdee03c4378fbef0b9e691badf3a773a24c8
umran <umran.atasoy2@gmail.com>
2023-04-28 00:36:45 +0300

    son

---
+++ AtmAPP.java
@@ -0,0 +1,20 @@
+public class AtmAPP {
+    public static void main(String[] args) {
+        System.out.println("Bu bir ATM projesidir.");
+//version 1
+        System.out.println("Java is easy.");
+        //version 2
+        System.out.println("learn Java earn money.");
+        //version 3
+        System.out.println("Java ogrenmek cok kolay");
+        //version 4
+        System.out.println("umran branchi yeni kodlar yazdi");
+//version umr1
+        System.out.println("bir kısım kodlar");
+        //umr2
+
+
+
+
+    }
+}
